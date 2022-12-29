bb36585d7a33a9819e3818959d280ccdb536f66f
lebansty <lebanstyv@gmail.com>
2022-12-29 19:21:25 +0530

    login error

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import UserContext from './userContext'
 import axios from 'axios'
 
@@ -10,6 +10,8 @@ function Nav() {
 
   let context =useContext(UserContext)
   const [admin,setAdmin]=useState(false)
+  const [user,setUser] =useState(false)
+  let navigate =useNavigate()
 let cartLength =context.cart.length
 let loadData=async()=>{
 try {
@@ -18,8 +20,12 @@ try {
       "id":window.localStorage.getItem("userid")
     }
   })
-  setAdmin(validation.data.admin)
-  console.log(validation)
+  if(validation.data.admin || validation.data.admin.user){
+    setAdmin(validation.data.admin)
+    setUser(validation.data.admin.user)
+  }
+
+ 
 } catch (error) {
   console.log(error)
 }
@@ -27,10 +33,16 @@ try {
 useEffect(()=>{
 loadData()
 },[])
+let logOut=()=>{
+window.localStorage.removeItem('userid');
+window.localStorage.removeItem('app-token');
+window.localStorage.removeItem('adminAccess');
+navigate("/login")
+}
   return (
     <>
       <nav className='banner'>
-        <h3 style={{ display: "inline" }}><Link to={'/'}>Sneak</Link></h3><Link to={'/cart-item'} className='cartIcon'><FontAwesomeIcon icon={faShoppingCart} /><div className='cart-count'>{cartLength?cartLength:0}</div></Link><span className='login'><Link to={"/login"}>Login/Signup</Link></span>{admin?<span className='product_update'><Link to={"/products"}>Update Products</Link></span>:null}
+        <h3 style={{ display: "inline" }}><Link to={'/'}>Sneak</Link></h3><Link to={'/cart-item'} className='cartIcon'><FontAwesomeIcon icon={faShoppingCart} /><div className='cart-count'>{cartLength?cartLength:0}</div></Link><span className='login'>{!admin && !user ?<Link to={"/login"}>Login/Signup</Link>:<span onClick={logOut}>Logout </span>}</span>{admin?<span className='product_update'><Link to={"/products"}>Update Products</Link></span>:null}
       </nav>
       <Outlet />
     </>
